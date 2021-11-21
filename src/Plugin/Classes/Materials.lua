70b15ffa5ef22ac9fd766fbff6fc1ffe38ff15dc
ArsonistD <hotdadog@gmail.com>
2021-11-20 23:43:42 -0500

    Show selected material

--- src/Plugin/Classes/Materials.lua
+++ src/Plugin/Classes/Materials.lua
@@ -4,12 +4,21 @@ local G = require(root.G)
 local module = {}
 module.__index = module
 
+local function getSelectedName(value)
+	for i, material in ipairs(G.modules["Data"].materials) do
+		if material[1] == value then
+			return material[3]
+		end
+	end
+end
+
 module.New = function(value)
 	local object = setmetatable({}, module)
 	
 	object.event = G.classes["Event"].New()
 	object.value = value
 	object.selected = value or ""
+	object.selectedName = getSelectedName(value)
 	
 	object.gui = Instance.new("Frame")
 	object.gui.Size = UDim2.new(1, 0, 0, 208)-- 184
@@ -36,6 +45,17 @@ module.New = function(value)
 	materialTitle.TextSize = 12
 	materialTitle.LayoutOrder = 1
 	materialTitle.Parent = materialTitleFrame
+
+	object.selectedLabel = Instance.new("TextLabel")
+	object.selectedLabel.Text = object.selectedName
+	object.selectedLabel.Font = Enum.Font.Arial
+	object.selectedLabel.TextSize = 12
+	object.selectedLabel.Position = UDim2.new(0, 121, 0, 0)
+	object.selectedLabel.Size = UDim2.new(1, -121, 1, 0)
+	object.selectedLabel.BackgroundColor3 = settings().Studio.Theme:GetColor(Enum.StudioStyleGuideColor.Item)
+	object.selectedLabel.BorderColor3 = settings().Studio.Theme:GetColor(Enum.StudioStyleGuideColor.Border)
+	object.selectedLabel.TextColor3 = settings().Studio.Theme:GetColor(Enum.StudioStyleGuideColor.MainText)
+	object.selectedLabel.Parent = materialTitleFrame
 	
 	local materialFrame = Instance.new("Frame")
 	materialFrame.BackgroundColor3 = settings().Studio.Theme:GetColor(Enum.StudioStyleGuideColor.Item)
@@ -44,13 +64,6 @@ module.New = function(value)
 	materialFrame.Parent = object.gui
 	materialFrame.LayoutOrder = 2
 	
-	local padding = Instance.new("UIPadding")
-	padding.PaddingBottom = UDim.new(0, 8)
-	padding.PaddingLeft = UDim.new(0, 8)
-	padding.PaddingRight = UDim.new(0, 0)
-	padding.PaddingTop = UDim.new(0, 8)
-	padding.Parent = object.gui
-
 	local materialPadding = Instance.new("UIPadding")
 	materialPadding.PaddingBottom = UDim.new(0, 8)
 	materialPadding.PaddingLeft = UDim.new(0, 8)
@@ -64,9 +77,17 @@ module.New = function(value)
 	gridLayout.SortOrder = Enum.SortOrder.LayoutOrder
 	gridLayout.Parent = materialFrame
 	gridLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
-	
+
+
+	object.selectedLabel.MouseEnter:Connect(function()
+		object.selectedLabel.Text = "Select material below..."
+	end)
+	object.selectedLabel.MouseLeave:Connect(function()
+		object.selectedLabel.Text = object.selectedName
+	end)
 	
 	object.imageButtons = {}
+	
 	for i, material in ipairs(G.modules["Data"].materials) do
 		local imageButton = Instance.new("ImageButton")
 		imageButton.Image = material[2]
@@ -124,7 +145,10 @@ module.Select = function(self, value)
 	self.selected = value or ""
 	self.imageButtons[self.selected].BackgroundColor3 = settings().Studio.Theme:GetColor(Enum.StudioStyleGuideColor.Button, Enum.StudioStyleGuideModifier.Default)
 	self.imageButtons[self.selected].BorderColor3 = settings().Studio.Theme:GetColor(Enum.StudioStyleGuideColor.Button, Enum.StudioStyleGuideModifier.Selected)
+
 	self.imageButtons[self.selected].ImageColor3 = Color3.new(1, 1, 1)
+	self.selectedName =  getSelectedName(value)
+	self.selectedLabel.Text = self.selectedName 
 end
 
 module.Destroy = function(self)
